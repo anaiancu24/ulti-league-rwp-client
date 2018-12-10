2005dbf3669c872146dc202e65804c4cd59d9eea
pvdvpebble <pvdvpebble@gmail.com>
2018-12-10 17:44:40 +0100

    basic homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,16 @@
-import * as React from 'react';
+import React, { PureComponent} from 'react';
 import HomePageBanner from './HomePageBanner'
-//import './HomePageStyles.css';
+import './HomePageStyles.css';
+import HomePageInfo from './HomePageInfo';
+import UpdateFeed from './UpdateFeed';
 
-export default class HomePage extends React.Component {
+export default class HomePage extends PureComponent {
   render() {
     return (
-      <div>
+      <div className="home-page">
         <HomePageBanner />
-        <h1>Ulti-League!!!!</h1>
-        <h2>Share your passion for ultimate-frisbee!</h2>
+        <HomePageInfo />
+        <UpdateFeed />
       </div>
     )
   }
