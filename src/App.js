df360a570627893a58ffd8342f3ce6370b526f79
pvdvpebble <pvdvpebble@gmail.com>
2018-12-11 12:05:38 +0100

    added logout page, logout action, currentUser reducer, constants, jwt.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
-import HomePage from './components/home/HomePage'
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import HomePage from './components/home/HomePage';
+import LogoutPage from './components/logout/LogoutPage';
 import './App.css';
 
 class App extends PureComponent {
@@ -9,6 +10,7 @@ class App extends PureComponent {
       <Router>
         <div className="App">
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/logout" component={LogoutPage} />
           <Route path="" render={() => <Redirect to="/" />} />
         </div>
       </Router>
---
+++ src/reducers/currentUser.js
@@ -0,0 +1,14 @@
+import { USER_LOGIN_SUCCESS, USER_LOGOUT } from '../actions/users'
+
+export default function (state = null, { type, payload }) {
+  switch (type) {
+    case USER_LOGIN_SUCCESS:
+      return payload
+
+    case USER_LOGOUT:
+      return null
+
+    default:
+      return state
+  }
+}
