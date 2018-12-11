f9017c72290f40a8c7a839a1a43df823df41e206
pvdvpebble <pvdvpebble@gmail.com>
2018-12-11 12:31:36 +0100

    added base components

---
+++ src/components/players/PlayerProfile.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export default function PlayerProfile(props) {
+  return (
+  <div className="player-profile">
+    <h2>Player Profile</h2>
+  </div>)
+}
---
+++ src/components/players/PlayerProfileContainer.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import PlayerProfile from './PlayerProfile';
+// import { loadPlayer } from '../../actions/players'
+
+
+class PlayerProfileContainer extends PureComponent {
+
+  /* componentDidMount() {
+    this.props.loadPlayer(Number(this.props.match.params.id))
+  } */
+
+  render() {
+    return (
+      <div className="player-profile-container">
+        <PlayerProfile />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  /* player: state.player,
+  playerId: state.event && state.event.id, */
+  authenticated: !!state.currentUser
+})
+
+const mapDispatchToProps = {
+  //loadEvent
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlayerProfileContainer)
