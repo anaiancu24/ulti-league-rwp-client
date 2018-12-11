b9b507924293b73f9211081772684b10d5091e36
pvdvpebble <pvdvpebble@gmail.com>
2018-12-11 12:08:34 +0100

    added currentUser reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,7 @@
-import { USER_LOGIN_SUCCESS, USER_LOGOUT } from '../actions/users'
+import {  USER_LOGOUT } from '../actions/users'
 
 export default function (state = null, { type, payload }) {
   switch (type) {
-    case USER_LOGIN_SUCCESS:
-      return payload
 
     case USER_LOGOUT:
       return null
