6dd9feef75bf63b03dec60fc86e4bffeb2df4784
pvdvpebble <pvdvpebble@gmail.com>
2018-12-13 11:10:23 +0100

    team, player fetches on profile and overview updated

--- src/components/players/PlayerProfile.js
+++ src/components/players/PlayerProfile.js
@@ -7,11 +7,14 @@ export default function PlayerProfile(props) {
       <h2>Super Awesome Profile of</h2>
       {
         player &&
-        <h2>{player.user.firstName} {player.user.lastName}</h2>
+        <div>
+          <h2>{player.user.firstName} {player.user.lastName}</h2>
+          <p>Plays for the {player.selectedTeam.team.name}</p>
+        </div>
       }
       {
         !player && 'Loading...'
       }
-      
+
     </div>)
 }
--- src/components/teams/TeamsOverview.js
+++ src/components/teams/TeamsOverview.js
@@ -1,8 +1,25 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function TeamsOverview (props) {
-  return (
-  <div className="teams-overview">
-    <h2>Teams Overview</h2>
-  </div>)
+    const { teams } = props
+    return (
+      <div className="teams-overview">
+        <h2>An overview of our Teams!</h2>
+        <ul>
+          {
+            (teams) && teams.map(team => {
+              return (
+                <li key={team.id}>
+                  <Link to={`/teams/${team.id}`}>{team.name} from {team.location}</Link>
+                </li>
+              )
+            })
+          }
+          {
+            !props.teams && 'Loading...'
+          }
+        </ul>
+  
+      </div>)
 }
