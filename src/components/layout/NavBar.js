2e36b5393a98107d1cc21b4b0d4e6af21c225984
pvdvpebble <pvdvpebble@gmail.com>
2018-12-17 15:15:10 +0100

    navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -11,40 +11,46 @@ const NavBar = (props) => {
   const { authenticated } = props
 
   return (
-    <div className="menu">
+    <nav>
+      <div className="logo" onClick={() => props.history.push('/')}>
+        <img src={logo} alt="ulti-league-logo"></img>
+      </div>
+
+      <div className="menu">
+        {
+          !authenticated &&
+          <div className="dropdown">
+            <button className="dropbtn">LOG IN / SIGN UP
+              <i className="fa fa-caret-down"></i>
+            </button>
+            <div className="dropdown-content">
+              <Link to={'/login'} style={{ textDecoration: "none" }}><p> LOG IN</p></Link>
+              <Link to={'/signup'} style={{ textDecoration: "none" }}><p> SIGN UP</p></Link>
+            </div>
+          </div>
+        }
+        {
+          authenticated &&
+          <Link to={'/logout'} style={{ textDecoration: "none" }}><p className='menuItem'>LOGOUT</p></Link>
+        }
+        {
+          authenticated &&
+          <Link to={'/Myaccount'} style={{ textDecoration: "none" }}><p className='menuItem'>YOUR ULTILEAGUE</p></Link>
+        }
+
+
+        <Link to={'/teams'} style={{ textDecoration: "none" }}> <p className='ulti-league'>ULTILEAGUE</p></Link>
+
+      </div>
+
       <div className="search">
         <input type="text" placeholder="..."></input>
         <img src={searchIcon} alt="search-icon"></img>
       </div>
 
-      {
-        !authenticated &&
-        <div className="dropdown">
-          <button class="dropbtn">LOG IN / SIGN UP
-            <i class="fa fa-caret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            <Link to={'/login'} style={ { textDecoration: "none"} }><p> LOG IN</p></Link>
-            <Link to={'/signup'} style={ { textDecoration: "none"} }><p> SIGN UP</p></Link>
-          </div>
-        </div>
-      }
-      {
-        authenticated &&
-        <Link to={'/Myaccount'}><p className='menuItem'>YOUR ULTILEAGUE</p></Link>
-      }
-      {
-        authenticated &&
-        <Link to={'/logout'}><p className='menuItem'>Logout</p></Link>
-      }
 
-      <Link to={'/teams'} style={ { textDecoration: "none"} }> <p className='menuItem'>ULTILEAGUE</p></Link>
 
-      <div className="logo" onClick={() => props.history.push('/')}>
-        <img src={logo} alt="ulti-league-logo"></img>
-      </div>
-
-    </div>
+    </nav>
 
 
   )
