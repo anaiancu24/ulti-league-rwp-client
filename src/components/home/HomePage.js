b217323e5223d4d7691e3ab704ac40dd9b56a592
pvdvpebble <pvdvpebble@gmail.com>
2018-12-18 16:06:00 +0100

    footer

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -18,33 +18,36 @@ class HomePage extends PureComponent {
 
   render() {
     return (
-      <div className="home-page">
-        <div className="hero">
-          <HomePageBanner />
-        </div>
-        <div className="promo">
-          <HomePagePromo />
-          <HomePageCard className="join-and-play-promo" tagName="join-and-play-tag" />
-          <HomePageCard className="support-team-promo" tagName="support-team-tag" />
-          <HomePageCard className="support-players-promo" tagName="support-players-tag" />
-          <HomePageCard className="follow-promo" tagName="follow-tag" />
-          <HomePageCard className="nominate-promo" tagName="nominate-tag" />
-          <HomePageCard className="owner-promo" tagName="owner-tag" />
-        </div>
-        <div className="description-promo">
-          <h2>Ultimate Frisbee is awesome!</h2>
-          <p>Unfortunately, it is also costly to participate in all the great tournaments. Travelling from city to city, country to country, or even contintent to continent ain’t cheap!</p>
-          <p>You can now support your favorite teams and help make the sport more popular. As a team co-owner you help your favorite teams play by investing in them. The money goes towards covering their travel expenses and you get to co-decide this season’s squad selection together with the coach and players!</p>
-        </div>
-        <div className="draft-promo">
-          <img className="draft-header" src={playerHeader} alt="ulti-league-top-players-header"></img>
-          <div className="be-you">
-            <img src={topPlayer2} alt="top-player-2" id="top-player-2"></img>
-            <img src={topPlayer1} alt="top-player-1"></img>
-            <img src={topPlayer3} alt="top-player-3" id="top-player-2"></img>
+      <div className="home">
+        <div className="home-page">
+          <div className="hero">
+            <HomePageBanner />
+          </div>
+          <div className="promo">
+            <HomePagePromo />
+            <HomePageCard className="join-and-play-promo" tagName="join-and-play-tag" />
+            <HomePageCard className="support-team-promo" tagName="support-team-tag" />
+            <HomePageCard className="support-players-promo" tagName="support-players-tag" />
+            <HomePageCard className="follow-promo" tagName="follow-tag" />
+            <HomePageCard className="nominate-promo" tagName="nominate-tag" />
+            <HomePageCard className="owner-promo" tagName="owner-tag" />
+          </div>
+          <div className="description-promo">
+            <h2>Ultimate Frisbee is awesome!</h2>
+            <p>Unfortunately, it is also costly to participate in all the great tournaments. Travelling from city to city, country to country, or even contintent to continent ain’t cheap!</p>
+            <p>You can now support your favorite teams and help make the sport more popular. As a team co-owner you help your favorite teams play by investing in them. The money goes towards covering their travel expenses and you get to co-decide this season’s squad selection together with the coach and players!</p>
+          </div>
+          <div className="draft-promo">
+            <img className="draft-header" src={playerHeader} alt="ulti-league-top-players-header"></img>
+            <div className="be-you">
+              <img src={topPlayer2} alt="top-player-2" id="top-player-2"></img>
+              <img src={topPlayer1} alt="top-player-1"></img>
+              <img src={topPlayer3} alt="top-player-3" id="top-player-2"></img>
+            </div>
           </div>
         </div>
       </div>
+
     )
   }
 }
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
-
+import youtube from '../../images/YOUTUBEICON.png'
+import facebook from '../../images/FACEBOOKICON.png'
+import insta from '../../images/INSTAICON.png'
+import twitter from '../../images/TWITTERICON.png'
 
 const Footer = (props) => {
   return (
     <div className="footer">
-      <h3>Contact</h3>
+      <h3>CONTACT</h3>
+      <div className="social-media-icons">
+      <img src={youtube} alt="youtube-icon"></img>
+      <img src={insta} alt="insta-icon"></img>
+      <img src={facebook} alt="facebook-icon"></img>
+      <img src={twitter} alt="twitter-icon"></img>
+      </div>
     </div>
   )
 }
